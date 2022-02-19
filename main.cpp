c95f2cc2a17f8bc5c068cb8e03aa4f582569ba99
risrael-1 <56362273+risrael-1@users.noreply.github.com>
2022-02-19 14:37:55 +0100

    Start project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    std::cout << "Bienvenue dans SVG Creator le super créateur de dessin !\n";
+    std::cout << "Pour commencer veuillez créer votre dessin :\n";
+    return 0;
+}
